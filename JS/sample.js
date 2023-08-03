42a27aae7af29ceb7a4629a9ba25dfeca314d9dd
Vaishali V <sweetchoco483@gmail.com>
2023-08-03 11:30:19 +0530

    update

--- JS/sample.js
+++ JS/sample.js
@@ -1,2 +1,21 @@
+//var
 var a = 50;
-console.log(a);
+console.log("This is the value declared in var type before altering: " + a);
+{
+    var a = 7;
+    console.log("This is the value declared in var type after altering: " + a);
+}
+console.log("This is the value declared in var type after block altering: " + a);
+
+//let
+let b = 10;
+console.log("This is the value declared in let type before altering: " + b);
+{
+    let b = 15;
+    console.log("This is the value declared in let type after altering: " + b);
+}
+console.log("This is the value declared in let type after block altering: " + b);
+
+//const
+const c = 20;
+console.log("This is the value declared in const type: " + c);
